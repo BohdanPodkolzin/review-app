4cb36b0da915e78aa0f93043758db92b58c064cb
szymptom <bpodkolzin@kse.org.ua>
2024-09-07 03:45:52 +0300

    OwnerRepository

---
+++ ReviewApp/ReviewApp/Repository/OwnerRepository.cs
@@ -0,0 +1,32 @@
+﻿using ReviewApp.Context;
+using ReviewApp.Models;
+using ReviewApp.Service;
+
+namespace ReviewApp.Repository
+{
+    public class OwnerRepository(PokemonContext context) : IOwnerRepository
+    {
+        private readonly PokemonContext _context = context;
+
+        public ICollection<Owner> GetOwners()
+            => _context.Owners.OrderBy(x => x.Id).ToList();
+
+        public Owner? GetOwner(int ownerId)
+            => _context.Owners.SingleOrDefault(o => o.Id == ownerId);
+
+        public ICollection<Owner> GetOwnersOfPokemon(int pokeId)
+            => _context.PokemonOwners
+                .Where(po => po.PokemonId == pokeId)
+                .Select(po => po.Owner)
+                .ToList();
+
+        public ICollection<Pokemon> GetPokemonsByOwner(int ownerId)
+            => _context.PokemonOwners
+                .Where(po => po.OwnerId == ownerId)
+                .Select(po => po.Pokemon)
+                .ToList();
+
+        public bool IsOwnerExists(int id)
+            => _context.Owners.Any(x => x != null && x.Id == id);
+    }
+}
---
+++ ReviewApp/ReviewApp/Service/IOwnerRepository.cs
@@ -0,0 +1,16 @@
+﻿using ReviewApp.Models;
+
+namespace ReviewApp.Service
+{
+    public interface IOwnerRepository
+    {
+        ICollection<Owner> GetOwners();
+        Owner? GetOwner(int ownerId);
+
+        ICollection<Owner> GetOwnersOfPokemon(int pokeId);
+        ICollection<Pokemon> GetPokemonsByOwner(int ownerId);
+
+        bool IsOwnerExists(int id);
+
+    }
+}
