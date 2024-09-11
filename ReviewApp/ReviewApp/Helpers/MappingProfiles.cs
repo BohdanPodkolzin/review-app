2ab3a825591e99d95014860ba2d02132f8507235
szymptom <bpodkolzin@kse.org.ua>
2024-09-12 01:21:16 +0300

    Category POST method Controller

--- ReviewApp/ReviewApp/Helpers/MappingProfiles.cs
+++ ReviewApp/ReviewApp/Helpers/MappingProfiles.cs
@@ -15,9 +15,16 @@ namespace ReviewApp.Helpers
             CreateMap<CategoryDto, Category>();
 
             CreateMap<Country, CountryDto>();
+            CreateMap<CountryDto,Country>();
+
             CreateMap<Owner, OwnerDto>();
+            CreateMap<OwnerDto, Owner>();
+
             CreateMap<Review, ReviewDto>();
+            CreateMap<ReviewDto, Review>();
+
             CreateMap<Reviewer, ReviewerDto>();
+            CreateMap<ReviewerDto, Reviewer>();
         }
     }
 }
